513796d6ff60817db7f52da96ced28986e006cec
Archit Gupta <architgupta@berkeley.edu>
2016-07-08 21:44:01 -0700

    master: Merged changes made in summer at Home (Added window layers) On branch master
    Changes committed:
          modified:   todolist.cpp

--- 2_Code/todolist.cpp
+++ 2_Code/todolist.cpp
@@ -342,8 +342,5 @@ SampleToDoList::SampleToDoList()
     new_todo_entry("Continue work on statistical mechanics");
     new_todo_entry("Complete the ToDo Application");
     //new_todo_entry("");
-    //new_todo_entry("");
-    //new_todo_entry("");
-    //new_todo_entry("");
     //new_todo_entry("This is a big todo list entry. It should span multiple lines and should let us check some of the multi-line functionality that we are trying to implement");
 }
